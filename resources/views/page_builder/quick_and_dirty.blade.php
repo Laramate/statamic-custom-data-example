e5b484e3fcf9e62d43c5a3319e2fde09a6f6b920
Chris Wolf <chris.wolf@laramate.de>
2026-03-22 13:13:49 +0100

    add quick and dirty implementation

---
+++ resources/views/page_builder/quick_and_dirty.blade.php
@@ -0,0 +1,11 @@
+@php
+    $products = new App\Actions\GetProductsAction($product_category)->handle();
+@endphp
+
+<section class="py-12 px-4 sm:px-6 lg:px-8 bg-gray-950 rounded-2xl">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        @foreach($products as $product)
+            <x-product-card :product="$product"></x-product-card>
+        @endforeach
+    </div>
+</section>
